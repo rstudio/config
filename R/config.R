740eb57917f654c99538d6a4d2b28884513a80e2
JJ Allaire <jj@rstudio.org>
2016-05-30 06:57:28 -0400

    DESCRIPTION and docs

--- R/config.R
+++
@@ -1,5 +0,0 @@
-
-
-#' @import yaml
-NULL
-
---
+++ R/package.R
@@ -0,0 +1,11 @@
+#' Manage Environment Specific Configuration Values
+#'
+#' Manage configuration values across multiple environments (e.g. development,
+#' test, production). Read values using a function that determines the current
+#' environment and returns the appropriate value.
+#'
+#' @import yaml
+#'
+#' @docType package
+#' @name config
+NULL
